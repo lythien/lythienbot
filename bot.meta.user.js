0f6d8f09e624f6a0ad5881f8882ef1b3b36d7043
lythien <blue.1631@yahoo.com.vn>
2016-05-26 10:52:53 +0700

    Add files via upload

---
+++ bot.meta.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         LyThienTop-bot A*
+// @namespace    http://slither.io/
+// @version      1.1.5 - LyThien.Top
+// @description  LyThienTop-bot A*
+// @author       Lý Thiện
+// @match        http://slither.io/
+// @require		   http://lythien.top/lythienbot-ver1/btree-complete.js
+// @require		   http://lythien.top/lythienbot-ver1/bot.astar.js
+// @require		   http://lythien.top/lythienbot-ver1/bot.gridsystem.js
+// @require		   http://lythien.top/lythienbot-ver1/bot.behaviors.js
+// @require		   http://lythien.top/lythienbot-ver1/bot.user.2.js
+// @grant        none
+// ==/UserScript==
