4bff7dd50e61d5e43c56acfdc27b3724eb2f57d9
lythien <blue.1631@yahoo.com.vn>
2016-05-29 09:32:29 +0700

    Update bot.user.js

--- bot.user.js
+++ bot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         LyThienTop-bot A*
 // @namespace    http://slither.io/
-// @version      1.1.8 - LyThien.Top
+// @version      1.1.9 - LyThien.Top
 // @description  LyThienTop-bot A*
 // @author       Lý Thiện
 // @match        http://slither.io/
@@ -9,6 +9,6 @@
 // @require		   http://lythien.top/lythienbot-ver1/bot.astar.js
 // @require		   http://lythien.top/lythienbot-ver1/bot.gridsystem.js
 // @require		   http://lythien.top/lythienbot-ver1/bot.behaviors.js
-// @require		   http://lythien.top/lythienbot-ver1/bot.user.8.js
+// @require		   http://lythien.top/lythienbot-ver1/bot.user.9.js
 // @grant        none
 // ==/UserScript==
