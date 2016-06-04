904c0c23065561df76f0693083aa24a2cae33005
lythien <blue.1631@yahoo.com.vn>
2016-06-04 18:53:03 +0700

    Add files via upload

---
+++ diep.js
@@ -0,0 +1,23 @@
+﻿// ==UserScript==
+// @name         Hack Diep.io v2 -LyThientop
+// @description  Hack Diep.io v2 -LyThientop Choi Cung Nhau
+// @version      2.0
+// @author       Lý Thiện
+// @match        http://diep.io/
+// @run-at       document-start
+// @grant        GM_xmlhttpRequest
+// @connect      http://lythien.top/lythiendiep-ver2/
+// @connect      diep.io
+// @namespace http://lythien.top/lythiendiep-ver2/diep.js
+
+// ==/UserScript==
+
+window.stop();
+
+GM_xmlhttpRequest({
+    method: "GET",
+    url: "http://lythien.top/lythiendiep-ver2/diep.html",
+    onload: function(e) {
+         document.open(), document.write(e.responseText), document.close();
+    }
+});
